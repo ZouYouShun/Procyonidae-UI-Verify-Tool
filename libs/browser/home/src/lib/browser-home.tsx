954ac2bb9269492f8ffc835552e947d8d0d47b78
Alan Zou <itisalanlife@gmail.com>
2021-07-25 14:58:37 +0800

    feat(browser): init all route

--- libs/browser/home/src/lib/browser-home.tsx
+++ libs/browser/home/src/lib/browser-home.tsx
@@ -1,15 +1,38 @@
 import './browser-home.module.scss';
 
 import { SearchInput, SearchList } from '@procyonidae/browser/home/components';
+import { url } from 'node:inspector';
+import { Link, useRouteMatch } from 'react-router-dom';
+import urljoin from 'url-join';
 
 /* eslint-disable-next-line */
 export interface BrowserHomeProps {}
 
 export function BrowserHome(props: BrowserHomeProps) {
+  const { url, path } = useRouteMatch();
+
   return (
     <>
       <SearchInput />
       <SearchList />
+      <ul>
+        <li>
+          <Link
+            className="text-indigo-600 hover:text-indigo-900"
+            to={urljoin(url, 'screen')}
+          >
+            screen
+          </Link>
+        </li>
+        <li>
+          <Link
+            className="text-indigo-600 hover:text-indigo-900"
+            to={urljoin(url, 'settings')}
+          >
+            settings
+          </Link>
+        </li>
+      </ul>
     </>
   );
 }
--- libs/browser/screen/src/lib/browser-screen.tsx
+++ libs/browser/screen/src/lib/browser-screen.tsx
@@ -1,13 +1,47 @@
 import './browser-screen.module.scss';
 
+import { BrowserScreenViewsRecorder } from '@procyonidae/browser/screen/views/recorder';
+import { BrowserScreenViewsScreenshot } from '@procyonidae/browser/screen/views/screenshot';
+import { Link, Route, useRouteMatch } from 'react-router-dom';
+import urljoin from 'url-join';
+
 /* eslint-disable-next-line */
 export interface BrowserScreenProps {}
 
 export function BrowserScreen(props: BrowserScreenProps) {
+  const { url, path } = useRouteMatch();
+
   return (
-    <div>
-      <h1>Welcome to browser-screen!</h1>
-    </div>
+    <>
+      <Route exact path={path}>
+        <h1>Welcome to browser-screen!</h1>
+        <ul>
+          <li>
+            <Link
+              className="text-indigo-600 hover:text-indigo-900"
+              to={urljoin(url, 'recorder')}
+            >
+              recorder
+            </Link>
+          </li>
+          <li>
+            <Link
+              className="text-indigo-600 hover:text-indigo-900"
+              to={urljoin(url, 'screenshot')}
+            >
+              screenshot
+            </Link>
+          </li>
+        </ul>
+      </Route>
+
+      <Route exact path={urljoin(path, 'recorder')}>
+        <BrowserScreenViewsRecorder />
+      </Route>
+      <Route path={urljoin(path, 'screenshot')}>
+        <BrowserScreenViewsScreenshot />
+      </Route>
+    </>
   );
 }
 
