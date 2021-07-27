fc54d1bbf111468cf2bd83a0d9870f3f603a14df
Alan Zou <itisalanlife@gmail.com>
2021-07-28 00:22:47 +0800

    feat(browser): [shared-hooks] init hooks

---
+++ libs/browser/shared/hooks/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'browser-shared-hooks',
+  preset: '../../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/browser/shared/hooks',
+};
---
+++ libs/browser/shared/hooks/src/index.ts
@@ -0,0 +1,4 @@
+export * from './useDebounce';
+export * from './useKeyboardMoveFocus';
+export * from './useRetry';
+export * from './useSleep';
