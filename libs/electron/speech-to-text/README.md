24e2ab658747a0ef52a850f0e112a67e5002b42e
Alan Zou <itisalanlife@gmail.com>
2021-08-20 01:19:43 +0800

    feat(electron): [speech-to-text] init speech to text

---
+++ libs/electron/speech-to-text/README.md
@@ -0,0 +1,7 @@
+# electron-speech-to-text
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test electron-speech-to-text` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/electron/speech-to-text/src/lib/electron-speech-to-text.ts
@@ -0,0 +1,54 @@
+import speech from '@google-cloud/speech';
+import { dialog } from 'electron';
+import fs from 'fs';
+
+export class SpeechToText {
+  async selectFile() {
+    const result = await dialog.showOpenDialog({ properties: ['openFile'] });
+
+    if (result.filePaths && result.filePaths?.length > 0) {
+      this.getTextFromAudio(result.filePaths[0]);
+    }
+  }
+
+  async getTextFromAudio(filePath: string) {
+    const client = new speech.SpeechClient();
+
+    const file = fs.readFileSync(filePath);
+
+    const [response] = await client.recognize({
+      audio: {
+        content: file,
+      },
+      config: {
+        enableAutomaticPunctuation: true,
+        sampleRateHertz: 16000,
+        encoding: 'LINEAR16',
+        languageCode: 'cmn-Hant-TW',
+        model: 'default',
+      },
+    });
+
+    if (response) {
+      const text = response.results
+        ?.map((x) => {
+          return x.alternatives
+            ?.map((alternatives) => alternatives.transcript)
+            .join('\n');
+        })
+        .join();
+      return text;
+    }
+
+    return '';
+  }
+
+  private static instance?: SpeechToText;
+
+  static getInstance(): SpeechToText {
+    if (!SpeechToText.instance) {
+      SpeechToText.instance = new SpeechToText();
+    }
+    return SpeechToText.instance;
+  }
+}
