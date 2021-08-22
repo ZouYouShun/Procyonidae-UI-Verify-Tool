8131aa7923039f3a9d2c4f0d1c59773dd34805d0
Alan Zou <itisalanlife@gmail.com>
2021-08-23 00:11:08 +0800

    feat(browser): [settings] init settings page layout

--- libs/browser/settings/feature/shell/src/lib/browser-settings.tsx
+++ libs/browser/settings/feature/shell/src/lib/browser-settings.tsx
@@ -6,53 +6,114 @@ import { BrowserSettingsFeatureSpeechToText } from '@procyonidae/browser/setting
 import { Link, Route, useRouteMatch } from 'react-router-dom';
 import urljoin from 'url-join';
 
+{
+  /* <Route exact path={path}>
+<h1>Welcome to browser-settings!</h1>
+<ul>
+  <li>
+    <Link
+      className="text-indigo-600 hover:text-indigo-900"
+      to={urljoin(url, 'account')}
+    >
+      account
+    </Link>
+  </li>
+  <li>
+    <Link
+      className="text-indigo-600 hover:text-indigo-900"
+      to={urljoin(url, 'snippets')}
+    >
+      snippets
+    </Link>
+  </li>
+  <li>
+    <Link
+      className="text-indigo-600 hover:text-indigo-900"
+      to={urljoin(url, 'speech-to-text')}
+    >
+      speech-to-text
+    </Link>
+  </li>
+</ul>
+</Route>
+<Route exact path={urljoin(path, 'account')}>
+<BrowserSettingsViewsAccount />
+</Route>
+<Route path={urljoin(path, 'snippets')}>
+<BrowserSettingsViewsSnippets />
+</Route>
+<Route path={urljoin(path, 'speech-to-text')}>
+<BrowserSettingsFeatureSpeechToText />
+</Route> */
+}
+
 /* eslint-disable-next-line */
 export interface BrowserSettingsProps {}
+
+const routes = [
+  {
+    path: 'snippets',
+    component: BrowserSettingsViewsSnippets,
+    data: {
+      title: 'Snippets',
+    },
+  },
+  {
+    path: 'speech-to-text',
+    component: BrowserSettingsFeatureSpeechToText,
+    data: {
+      title: 'Speech',
+    },
+  },
+  {
+    path: 'account',
+    component: BrowserSettingsViewsAccount,
+    data: {
+      title: 'Account',
+    },
+  },
+];
 
 export function BrowserSettings(props: BrowserSettingsProps) {
   const { url, path } = useRouteMatch();
 
   return (
-    <>
-      <Route exact path={path}>
-        <h1>Welcome to browser-settings!</h1>
+    <section className="container">
+      <nav className="w-25 p-3">
+        <header className="d-flex pb-3">
+          <h2>
+            <Link to={url}>AstralStar</Link>
+          </h2>
+        </header>
         <ul>
-          <li>
-            <Link
-              className="text-indigo-600 hover:text-indigo-900"
-              to={urljoin(url, 'account')}
-            >
-              account
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-indigo-600 hover:text-indigo-900"
-              to={urljoin(url, 'snippets')}
-            >
-              snippets
-            </Link>
-          </li>
-          <li>
-            <Link
-              className="text-indigo-600 hover:text-indigo-900"
-              to={urljoin(url, 'speech-to-text')}
-            >
-              speech-to-text
-            </Link>
-          </li>
+          {routes.map((route) => {
+            return (
+              <li key={route.path}>
+                <Link
+                  className="text-indigo-600 hover:text-indigo-900"
+                  to={urljoin(url, route.path)}
+                >
+                  {route.data.title}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
-      </Route>
-      <Route exact path={urljoin(path, 'account')}>
-        <BrowserSettingsViewsAccount />
-      </Route>
-      <Route path={urljoin(path, 'snippets')}>
-        <BrowserSettingsViewsSnippets />
-      </Route>
-      <Route path={urljoin(path, 'speech-to-text')}>
-        <BrowserSettingsFeatureSpeechToText />
-      </Route>
-    </>
+      </nav>
+      <main className="py-3 pl-0 w-100">
+        <Route exact path={path}>
+          <h1>Welcome to browser-settings!</h1>
+        </Route>
+        {routes.map((route) => {
+          const Component = route.component;
+          return (
+            <Route exact path={urljoin(path, route.path)}>
+              <Component />
+            </Route>
+          );
+        })}
+      </main>
+    </section>
   );
 }
 
